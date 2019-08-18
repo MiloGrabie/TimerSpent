6e4404ec866eb7c14dff949f1d22d8443aa837f1
Milo Grabié <grabiemilo@gmail.com>
2019-08-18 20:27:53 +0200

    Ajoutez des fichiers projet.

---
+++ TimerSpent/MainWindow.xaml.cs
@@ -0,0 +1,349 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.IO;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using Newtonsoft.Json;
+using System.Web.Script.Serialization;
+
+namespace TimerSpent
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string docPath;
+        private List<CustomTimeStamp> text;
+        private WordsManagerLib.WordsManager wordsManager;
+        NotifyIcon notifyIcon = new NotifyIcon();
+        private bool IsRecording;
+        DispatcherTimer dispatcherTimer;
+        string actualTime;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            const string userRoot = "HKEY_CURRENT_USER";
+            const string subkey = @"Software\Microsoft\OneDrive";
+            const string keyName = userRoot + "\\" + subkey;
+
+            string onedrivePath = (string)Microsoft.Win32.Registry.GetValue(keyName,
+            "UserFolder",
+            "Return this default if NoSuchName does not exist.");
+
+            notifyIcon.Icon = Properties.Resources.IconOff;
+            notifyIcon.Visible = true;
+            ContextMenu contextMenu = new ContextMenu();
+            MenuItem menuItemClose = new MenuItem { Text = "Close" };
+            menuItemClose.Click += MenuItem_Click_Close;
+            contextMenu.MenuItems.Add(menuItemClose);
+
+            MenuItem menuItemEdit = new MenuItem { Text = "Edit" };
+            menuItemEdit.Click += MenuItemEdit_Click;
+            contextMenu.MenuItems.Add(menuItemEdit);
+
+            MenuItem menuItemELL = new MenuItem { Text = "Erase last line" };
+            menuItemELL.Click += MenuItemELL_Click; ;
+            contextMenu.MenuItems.Add(menuItemELL);
+
+            notifyIcon.ContextMenu = contextMenu;
+            notifyIcon.DoubleClick += Click_Icon_ON;
+
+            docPath = System.IO.Path.Combine(onedrivePath, @"Documents\TimeSpent\TimeFile.json");
+            Read();
+            wordsManager = new WordsManagerLib.WordsManager();
+
+            //Temp();
+            MainCall(new object(), new EventArgs());
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Tick += new EventHandler(MainCall);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 60);
+            dispatcherTimer.Start();
+
+            notifyIcon.Text = getAllTime();
+            this.Hide();
+        }
+
+        private void MenuItemELL_Click(object sender, EventArgs e)
+        {
+            Read();
+            if (text.Last().Balise.Contains("Stop"))
+            {
+                text.Remove(text.Last());
+                text.Reverse();
+                text.Remove(text.ToList()[1]);
+                text.Reverse();
+                Save();
+            }
+        }
+
+        /*private void RemoveLine(int index) // reverse index (last line is 0)
+        {
+            if (index == -1) // remove last
+            {
+                text.Remove(text.Split('\n').Reverse().ToList()[0].Count());
+            }
+            else
+            {
+                for (int i = 1; i <= index+1; i++)
+                {
+                    int count = System.Text.RegularExpressions.Regex.Split(text, "Time").Select(b=>b += "Time").LastOrDefault().Count();
+                    text.Remove(count);
+                }
+            }
+        }*/
+
+        private void MenuItemEdit_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(docPath);
+        }
+
+        /*private void Temp()
+        {
+            Save();
+            var parsedText = System.Text.RegularExpressions.Regex.Split(text, "Time").Select(b => b += "Time");
+            List<CustomTimeStamp> stampList = new List<CustomTimeStamp>();
+            foreach (var item in parsedText)
+            {
+                var splitedString = item.Split(' ');
+                string a = splitedString[0].Split('\n').LastOrDefault();
+                try
+                {
+                    stampList.Add(new CustomTimeStamp
+                    {
+                        Date = splitedString[0].Split('\n').LastOrDefault(),
+                        Time = splitedString[1],
+                        Balise = splitedString[2],
+                    });
+                }
+                catch
+                {
+
+                }
+            }
+
+            string path = @"C:\Users\Milo.Valérie-Toshiba\OneDrive\Documents\Code\RH Solution\repos\TimerSpent\TimerSpent\Resources\json1.json";
+            //var result = JsonConvert.DeserializeObject<CustomTimeStamp>(path);
+             
+            var j = new JavaScriptSerializer().Serialize(stampList);
+            System.IO.File.WriteAllText(path, j);
+            using (StreamReader r = new StreamReader(path))
+            {
+                string json = r.ReadToEnd();
+                List<CustomTimeStamp> items = JsonConvert.DeserializeObject<List<CustomTimeStamp>>(json);
+            }
+
+
+        }*/
+
+        private void Save()
+        {
+            var jsonString = new JavaScriptSerializer().Serialize(text);
+            File.WriteAllText(docPath, jsonString);
+        }
+
+        private void Read()
+        {
+            using (StreamReader r = new StreamReader(docPath))
+            {
+                string json = r.ReadToEnd();
+                text = JsonConvert.DeserializeObject<List<CustomTimeStamp>>(json);
+            }
+        }
+
+        private void MenuItem_Click_Close(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void MainCall(object sender, EventArgs e)
+        {
+            Read();
+            if (text.LastOrDefault().Balise.Contains("StartTime"))
+            {
+                Textbox.Text = "Recording";
+                IsRecording = true;
+                notifyIcon.Icon = Properties.Resources.IconOn;
+            }
+            actualTime = getAllTime();
+            notifyIcon.Text = actualTime;
+        }
+
+        private void Click_Icon_ON(object sender, EventArgs e)
+        {
+            if(IsRecording)
+            {
+                Button_Click_1(new object(), new RoutedEventArgs());
+                notifyIcon.Icon = Properties.Resources.IconOff;
+                IsRecording = false;
+            }
+            else
+            {
+                Button_Click(new object(), new RoutedEventArgs());
+                notifyIcon.Icon = Properties.Resources.IconOn;
+                IsRecording = true;
+            }
+        }
+        
+        private void Button_Click_1(object sender, RoutedEventArgs e)   // Stop
+        {
+            Read();
+            if (!text.LastOrDefault().Balise.Contains("StopTime"))
+            {
+                Textbox.Text = "Not recording";
+                text.Add(new CustomTimeStamp
+                {
+                    Date = DateTime.Now.Date.ToString("dd/MM/yyyy"),
+                    Time = DateTime.Now.ToString("HH:mm:ss"),
+                    Balise = "StopTime"
+                });
+                Save();
+                IsRecording = false;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) // Start
+        {
+            Read();
+            if (!text.LastOrDefault().Balise.Contains("StartTime"))
+            {
+                text.Add(new CustomTimeStamp
+                {
+                    Date = DateTime.Now.Date.ToString("dd/MM/yyyy"),
+                    Time = DateTime.Now.ToString("HH:mm:ss"),
+                    Balise = "StartTime"
+                });
+                Textbox.Text = "Recording";
+                Save();
+                IsRecording = true;
+            }
+        }
+
+        private void Calcul_Button(object sender, RoutedEventArgs e)   // calcul line
+        {
+            string h = getAllTime();
+            TimeResult.Text = h;
+        }
+
+        private string getAllTime()
+        {
+            Read();
+            double hour = 0;
+            for (int i = 0; i < text.Count; i=i+2)
+            {
+                CustomTimeStamp startItem = text[i];
+                try
+                {
+                    CustomTimeStamp stopItem = text[i + 1];
+                    if (startItem.Balise == "StartTime")
+                    {
+                        if (stopItem.Balise == "StopTime")
+                        {
+                            hour += (
+                            Convert.ToDateTime(stopItem.Date + " " + stopItem.Time) -
+                            Convert.ToDateTime(startItem.Date + " " + startItem.Time)
+                            ).TotalHours;
+                        }
+                        else { }
+                    }
+                    else
+                    {
+                        hour += (DateTime.Now - Convert.ToDateTime(startItem.Date + " " + startItem.Time))
+                            .TotalHours;
+                    }
+                }
+                catch
+                {
+                    hour += (DateTime.Now - Convert.ToDateTime(startItem.Date + " " + startItem.Time))
+                        .TotalHours;
+                }
+            }
+            TimeSpan timeSpan = TimeSpan.FromHours(hour);
+            int hours = (timeSpan.Days * 24) + timeSpan.Hours;
+            int min = timeSpan.Minutes;
+            string h = hours.ToString() + " h - " + min.ToString() + " min";
+            return h;
+
+            /*List<DateTime> startList = text.Where(a => a.Balise.Contains("Start")).Select(a=>Convert.ToDateTime(a.Date+" "+a.Time)).ToList();
+            List<DateTime> stopList = text.Where(a => a.Balise.Contains("Stop")).Select(a=>Convert.ToDateTime(a.Date + " " + a.Time)).ToList();
+
+            double hour = 0;
+            foreach (DateTime stamp in startList)
+            {
+                try
+                {
+                    hour += (stopList[startList.IndexOf(stamp)] - stamp).TotalHours;
+                }
+                catch (Exception)
+                {
+                    hour += (DateTime.Now - stamp).TotalHours;
+                }
+
+            }*/
+
+        }
+
+        public List<DateTime> treatLine(List<CustomTimeStamp> list)
+        {
+            List<DateTime> timeStamp = new List<DateTime>();
+            foreach (CustomTimeStamp stamp in list)
+            {
+                timeStamp.Add(Convert.ToDateTime(stamp.Date));
+            }
+            return timeStamp;
+        }
+
+        /*public void CalculMoyenne()
+        {
+            Read();
+            wordsManager.stockLine(text, true);
+            List<WordsManagerLib.Line> start = wordsManager.getLine("Start");
+            List<DateTime> startList = treatLine(start);
+            List<WordsManagerLib.Line> stop = wordsManager.getLine("Stop");
+            List<DateTime> stopList = treatLine(stop);
+
+            Dictionary<DateTime,double> deltaTime = new Dictionary<DateTime, double>();
+            DateTime actualDay = new DateTime();
+            double timeSpans = 0;
+            foreach (var item in startList)
+            {
+                timeSpans += (stopList[startList.IndexOf(item)] - item).TotalHours;
+                if (actualDay != item.Date)
+                {
+                    actualDay = item.Date;
+                    deltaTime.Add(actualDay, timeSpans);
+                    timeSpans = 0;
+                }
+            }
+            double count = Convert.ToDouble(deltaTime.Count());
+            double total = 0;
+            foreach (var item in deltaTime)
+            {
+                total += item.Value;
+            }
+            double average = total / count;
+        }*/
+
+       
+    }
+
+    public class CustomTimeStamp
+    {
+        public string Date { set; get; }
+        public string Time { set; get; }
+        public string Balise { set; get; }
+    }
+}
---
+++ TimerSpent/WordsManager.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace WordsManagerLib
+{
+    public class WordsManager
+    {
+        public List<Line> MainTextLine;
+        public string text;
+        public PdfReader reader;
+        public string pdfPath;
+
+        public WordsManager()
+        {
+            MainTextLine = new List<Line>();
+        }
+
+        public string loadPdf(string path)
+        {
+            pdfPath = path;
+            text = ExtractTextFromPdf(path);
+            reader = new PdfReader(path);
+            Console.WriteLine("Stockage...");
+            if (text.Length < 10)
+            {
+                throw new Exception("Text length under 10");
+            }
+            MainTextLine = stockLine(text);
+            return text;
+        }
+
+        public string ExtractTextFromPdf(string path)
+        {
+            using (PdfReader reader = new PdfReader(path))
+            {
+                StringBuilder text = new StringBuilder();
+
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+                }
+
+                return text.ToString();
+            }
+        }
+
+        public List<Line> getTextFromRectangle(int x, int y, int w, int h)
+        {
+            System.util.RectangleJ rect0 = new System.util.RectangleJ(x, y, w, h);
+            RenderFilter[] filter = { new RegionTextRenderFilter(rect0) };
+            ITextExtractionStrategy strategy;
+            StringBuilder sb = new StringBuilder();
+            strategy = new FilteredTextRenderListener(new LocationTextExtractionStrategy(), filter);
+            sb.AppendLine(PdfTextExtractor.GetTextFromPage(reader, 1, strategy));
+
+            List<Line> line = stockLine(sb.ToString());
+            return line;
+        }
+
+        public iTextSharp.text.Rectangle getPositionOfString(string searchText)
+        {
+            //Create an instance of our strategy
+            var t = new WordLocationTextExtractionStrategy(searchText);
+
+            //Parse page 1 of the document above
+            var ex = PdfTextExtractor.GetTextFromPage(reader, 1, t);
+            return t.myPoints[0].Rect;
+        }
+
+        // return all the line containing the sentence (stc), if getUnderLine true, return line with it line+1
+        public List<Line> getLine(string stc, bool lower = false, bool getUnderLine = false) // TODO All in one line
+        {
+            if (lower) stc = stc.ToLower();
+            List<Line> result = new List<Line>();
+            foreach (Line line in MainTextLine)
+            {
+                if (line.lineContent.ToLower().Contains(stc) && getUnderLine)
+                {
+                    Line underLine = MainTextLine.Find(i => i.lineNumber == line.lineNumber + 1);
+                    line.lineContent = line.lineContent + underLine.lineContent;
+                    foreach (Word word in underLine.word)
+                    {
+                        line.word.Add(word);
+                    }
+                    result.Add(line);
+                }
+                else if (line.lineContent.ToLower().Contains(stc) && lower)
+                {
+                    result.Add(line);
+                }
+                else if (line.lineContent.Contains(stc))    // For future improvement
+                {
+                    result.Add(line);
+                }
+            }
+            if (result.Count() == 0)
+            {
+                throw new Exception("No line found.");
+            }
+            return result;
+        }
+
+
+        public List<Line> stockLine(string localText, bool saveLine = false)
+        {
+            List<Line> localLine = new List<Line>();
+            string[] lines = localText.Replace("\r", "").Split('\n');
+            for (int i = 0; i < lines.Length; i++)
+            {
+                try
+                {
+                    localLine.Add(new Line() { lineContent = lines[i], lineIndex = text.IndexOf(lines[i][0]), lineNumber = i });
+                }
+                catch (Exception)
+                {
+                    localLine.Add(new Line() { lineContent = lines[i], lineIndex = 0, lineNumber = i });
+                }
+                string[] words = lines[i].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                foreach (string w in words)
+                {
+                    localLine[i].word.Add(new Word() { wordName = w, indexInText = localText.IndexOf(w), indexInLine = Array.IndexOf(words, w) });
+                }
+            }
+            if (saveLine)
+            {
+                MainTextLine = localLine;
+            }
+            return localLine;
+        }
+
+        public void realeasePdf()
+        {
+            reader = null;
+        }
+    }
+
+    public class WordLocationTextExtractionStrategy : LocationTextExtractionStrategy
+    {
+        //Hold each coordinate
+        public List<RectAndText> myPoints = new List<RectAndText>();
+
+        //The string that we're searching for
+        public String TextToSearchFor { get; set; }
+
+        // Part to find the right word by recording once we have the right first char until the length of the searched text
+        public int listen = -1;
+        public string word = "";
+
+        //How to compare strings
+        public System.Globalization.CompareOptions CompareOptions { get; set; }
+
+        public WordLocationTextExtractionStrategy(String textToSearchFor, System.Globalization.CompareOptions compareOptions = System.Globalization.CompareOptions.None)
+        {
+            this.TextToSearchFor = textToSearchFor;
+            this.CompareOptions = compareOptions;
+        }
+
+        //Automatically called for each chunk of text in the PDF
+        public override void RenderText(TextRenderInfo renderInfo)
+        {
+            base.RenderText(renderInfo);
+
+            string text = renderInfo.GetText();
+
+            //See if the current chunk contains the text
+            var startPosition = System.Globalization.CultureInfo.CurrentCulture.CompareInfo.IndexOf(renderInfo.GetText(), this.TextToSearchFor, this.CompareOptions);
+
+            if (listen >= this.TextToSearchFor.Count())
+            {
+                if (this.TextToSearchFor == word)
+                {
+                    //Grab the individual characters
+                    var chars = renderInfo.GetCharacterRenderInfos().Skip(startPosition).Take(this.TextToSearchFor.Length).ToList();
+
+                    //Grab the first and last character
+                    var firstChar = chars.First();
+                    var lastChar = chars.Last();
+
+
+                    //Get the bounding box for the chunk of text
+                    var bottomLeft = firstChar.GetDescentLine().GetStartPoint();
+                    var topRight = lastChar.GetAscentLine().GetEndPoint();
+
+                    //Create a rectangle from it
+                    var rect = new iTextSharp.text.Rectangle(
+                                                            bottomLeft[Vector.I1],
+                                                            bottomLeft[Vector.I2],
+                                                            topRight[Vector.I1],
+                                                            topRight[Vector.I2]
+                                                            );
+
+                    //Add this to our main collection
+                    this.myPoints.Add(new RectAndText(rect, word));
+                    listen = -1;
+                    word = "";
+                    return;
+                }
+                listen = -1;
+                word = "";
+            }
+            else if (listen > -1)
+            {
+                word += renderInfo.GetText();
+                listen += renderInfo.GetText().Count();
+            }
+            else if (this.TextToSearchFor[0] == renderInfo.GetText()[0])
+            {
+                listen = 0;
+                word += renderInfo.GetText();
+                listen += renderInfo.GetText().Count();
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    public class RectAndText
+    {
+        public iTextSharp.text.Rectangle Rect;
+        public string Text;
+        public RectAndText(iTextSharp.text.Rectangle rect, string text)
+        {
+            this.Rect = rect;
+            this.Text = text;
+        }
+    }
+
+    public class Line
+    {
+        public string lineContent = "";
+        public int lineIndex = -1;
+        public int lineNumber = -1;
+        public List<Word> word;
+
+        public Line()
+        {
+            word = new List<Word>();
+        }
+    }
+
+    public class Word   // Useful for quick check of word
+    {
+        public string wordName = "";
+        public int indexInText = -1;    // index of the first letter among the other letter
+        public int indexInLine = -1;   // index of the word in the line
+    }
+
+}
